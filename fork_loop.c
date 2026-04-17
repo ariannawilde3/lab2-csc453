6d9af995561aef510e69ea03e8957094cb4de3aa
Arianna Wilde <arianna@wildefamily.net>
2026-04-17 16:30:40 -0700

    gitignore and fork part 1

---
+++ fork_loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void) {
+    int i = 0;
+
+    while (fork() && i < 5) {
+        wait (NULL);
+        printf("Hey!");
+        i++;
+    }
+
+    printf("%d", i);
+    return 0
+}
