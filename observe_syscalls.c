03b257e2babb8cb756cd0aca6da35ea23c07a67a
Arianna Wilde <arianna@wildefamily.net>
2026-04-17 16:48:28 -0700

    adding part 2 of lab

---
+++ observe_syscalls.c
@@ -0,0 +1,40 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+
+int main(void) {
+
+    /* step 1 */ 
+    pid_t = getpid();
+    printf("PID: %d\n", pid);
+
+    /* step 2 */ 
+    int fileDescriptor = open("/etc/hostname", O_RDONLY);
+    if (fileDescriptor < 0) {
+        perror("open");
+        return 1;
+    }
+
+    /* step 3 */ 
+    char buffer[256];
+    ssize_t n = read(fileDescriptor, buffer, sizeof(buffer) -1);
+    if (n < 0) {
+
+        perror("read");
+        close(fileDescriptor);
+        return 1;
+    }
+
+    buffer[n] = '\0';
+
+    /* step 4 */ 
+    close(fileDescriptor);
+
+    /* step 5 */ 
+    write(1, "Hostname: ", 10);
+    write(1, buffer, n);
+
+    return 0;
+}
